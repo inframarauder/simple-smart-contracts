0cee0948f68f2738385247aa3f58b72cccdb7b3b
subhasis020299 <subhasisdas125@gmail.com>
2022-03-26 15:57:24 +0530

    added compile and deploy scripts

---
+++ deploy.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { abi, evm } = require("./compile");
+
+//setup HD-Wallet provider:
+const accountMnemonic = process.env.ACCOUNT_MNEMONIC;
+const infuraBaseUrl = process.env.INFURA_BASE_URL;
+const provider = new HDWalletProvider(accountMnemonic, infuraBaseUrl);
+
+//setup web3 instance:
+const web3 = new Web3(provider);
+
+(async () => {
+	//get list of accounts from provider
+	const account = (await web3.eth.getAccounts())[0];
+	console.log(`Using account ${account} for deployment....`);
+
+	//deploy the contract and print address:
+	const contract = await new web3.eth.Contract(JSON.parse(abi))
+		.deploy({
+			data: evm.bytecode.object,
+		})
+		.send({
+			gas: "1000000",
+			from: account,
+		});
+
+	console.log(`Contract deployed to address : ${contract.options.address}`);
+
+	//stop the provider engine to prevent script from hanging post deployment
+	provider.engine.stop();
+})();
