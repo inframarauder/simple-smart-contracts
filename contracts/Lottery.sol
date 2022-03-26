0e1f4f77cbc6e0477df54c698a33c89260bbbebe
subhasis020299 <subhasisdas125@gmail.com>
2022-03-27 01:07:28 +0530

    added comprehensive testing for pickWinner()

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,11 +3,14 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Lottery{
-    address manager;
+    address manager; 
     address[] players;
+    string lotteryId;
+    mapping(string => address) lotteryWinners;
 
-    constructor(){
+    constructor(string memory _lotteryId){
         manager = msg.sender;
+        lotteryId = _lotteryId;
     }
 
     modifier restricted(){
@@ -15,34 +18,44 @@ contract Lottery{
         _;
     }
 
+
     function getManager() public view returns(address){
         return manager;
+    }
+
+    function getLotteryId() public view returns(string memory){
+        return lotteryId;
     }
 
     function getPlayers() public view returns(address[] memory){
         return players;
     }
 
+    function getPrizePool() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function getWinnerByLotteryId(string memory _lotteryId) public view returns(address){
+        return lotteryWinners[_lotteryId];
+    }
+
+    function getRandom() private view returns(uint){
+       return uint(sha256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+    }
+
     function enterLottery() public payable {
         require(msg.sender != manager,"Manager cant enter lottery"); 
-        require(msg.value >= 100 wei,"Minimum 100 wei needed to enter"); 
+        require(msg.value >= 0.1 ether,"Minimum 0.1 eth needed to enter"); 
         
         players.push(msg.sender); 
     }
 
-     function getPrizePool() public view returns(uint){
-        return address(this).balance;
-    }
-
-    function random() private view returns(uint){
-       return uint(sha256(abi.encodePacked(block.difficulty, block.timestamp, players)));
-    }
-
     function pickWinner() public restricted {
         uint prizePool = getPrizePool();
-        uint index = random() %  players.length;
+        uint index = getRandom() %  players.length;
         address payable winner = payable(players[index]);
         winner.transfer(prizePool);
+        lotteryWinners[lotteryId] = winner;
         players = new address[](0);
     }
 }
--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,18 +1,20 @@
 const assert = require("assert");
 const Web3 = require("web3");
 const ganache = require("ganache-cli");
+const uuid = require("uuid").v4;
 const { abi, evm } = require("../compile");
 
-const web3 = new Web3(ganache.provider());
+const web3 = new Web3(ganache.provider({ gasLimit: "50000000" }));
 
 let lottery, accounts;
+const defaultLotteryId = uuid();
 
 beforeEach(async () => {
 	accounts = await web3.eth.getAccounts();
 
 	lottery = await new web3.eth.Contract(abi)
-		.deploy({ data: evm.bytecode.object })
-		.send({ gas: "1000000", from: accounts[0] });
+		.deploy({ data: evm.bytecode.object, arguments: [defaultLotteryId] })
+		.send({ gas: "5000000", from: accounts[0] });
 });
 
 describe("Lottery Test Suite", () => {
@@ -25,11 +27,16 @@ describe("Lottery Test Suite", () => {
 		assert.equal(accounts[0], manager);
 	});
 
+	it("returns the lotteryId", async () => {
+		const lotteryId = await lottery.methods.getLotteryId().call();
+		assert.equal(defaultLotteryId, lotteryId);
+	});
+
 	it("does not let manager enter the lottery", async () => {
 		try {
 			await lottery.methods.enterLottery().send({
 				from: accounts[0],
-				value: "100",
+				value: web3.utils.toWei("0.1", "ether"),
 			});
 			assert(false);
 		} catch (error) {
@@ -37,17 +44,17 @@ describe("Lottery Test Suite", () => {
 		}
 	});
 
-	it("does not let a player enter the lottery with less than 100 wei", async () => {
+	it("does not let a player enter the lottery with less than 0.1 ether", async () => {
 		try {
 			await lottery.methods.enterLottery().send({
 				from: accounts[1],
-				value: "10",
+				value: web3.utils.toWei("0.05", "ether"),
 			});
 			assert(false);
 		} catch (error) {
 			assert.equal(
 				true,
-				error.message.includes("Minimum 100 wei needed to enter")
+				error.message.includes("Minimum 0.1 eth needed to enter")
 			);
 		}
 	});
@@ -55,7 +62,7 @@ describe("Lottery Test Suite", () => {
 	it("allows a player to enter the lottery", async () => {
 		await lottery.methods.enterLottery().send({
 			from: accounts[1],
-			value: "100",
+			value: web3.utils.toWei("0.1", "ether"),
 		});
 
 		const players = await lottery.methods.getPlayers().call();
@@ -75,5 +82,41 @@ describe("Lottery Test Suite", () => {
 				error.message.includes("Only manager can call this method")
 			);
 		}
+	});
+
+	it("sends prize pool amount to winner and resets the players array", async () => {
+		//enter two players into the lottery:
+		const initialBalance1 = await web3.eth.getBalance(accounts[1]);
+		await lottery.methods.enterLottery().send({
+			from: accounts[1],
+			value: web3.utils.toWei("0.1", "ether"),
+		});
+
+		const initialBalance2 = await web3.eth.getBalance(accounts[2]);
+		await lottery.methods.enterLottery().send({
+			from: accounts[2],
+			value: web3.utils.toWei("0.1", "ether"),
+		});
+
+		//pick a winner:
+		await lottery.methods.pickWinner().send({
+			from: accounts[0], //using manager account
+		});
+
+		//determine winner :
+		const winner = await lottery.methods
+			.getWinnerByLotteryId(defaultLotteryId)
+			.call();
+		const winnerBalance = await web3.eth.getBalance(winner);
+		const condition =
+			winner === accounts[1]
+				? winnerBalance > initialBalance1
+				: winnerBalance > initialBalance2;
+
+		assert(condition);
+
+		//ensure players is reset:
+		const players = await lottery.methods.getPlayers().call();
+		assert.equal(0, players.length);
 	});
 });
